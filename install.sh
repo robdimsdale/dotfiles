932cbd536ef419e1d74cbe6e510e0bf798d22f12
Rob Dimsdale <robert.dimsdale@gmail.com>
2015-07-29 22:07:43 -0700

    Add top-level install script.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Run all dotfiles installers.
+
+set -eu
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+"${MY_DIR}/homebrew/install.sh"
+"${MY_DIR}/bash-it/install.sh"
+"${MY_DIR}/vim/install.sh"
