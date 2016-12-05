2eaf7c9ba769f74dc0cca736342f8cd078b9ec04
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2016-12-04 19:17:27 -0800

    Do not show ruby prompt in bash-it themes.

--- bash-it/custom/800-bash-it-prompt.bash
+++ bash-it/custom/800-bash-it-prompt.bash
@@ -2,6 +2,11 @@
 
 set -eu
 
+# Overwrite the ruby prompt to remove it without editing the built-in themes.
+function ruby_version_prompt() {
+  echo -e "\b" # echo backspace to avoid double whitespace.
+}
+
 export THEME_SHOW_CLOCK_CHAR=false
 
 set +eu
