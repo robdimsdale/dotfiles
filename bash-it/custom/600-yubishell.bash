1bd1a85f6cd368642dfb5384d93db508fc01a47a
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2017-05-23 22:00:53 -0700

    Make yubishell function more resilient to bad SSH_AUTH_SOCK.

--- bash-it/custom/600-yubishell.bash
+++ bash-it/custom/600-yubishell.bash
@@ -2,6 +2,7 @@ yubishell() {
   (
     eval $(keychain --agents "gpg" --quiet --eval);
     gpg-connect-agent updatestartuptty /bye;
+    export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
     export PS1='\[\e[31m\][YUBI]\[\e[0m\] \$ ';
     bash
   )
