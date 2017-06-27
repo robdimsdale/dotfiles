4e1f91aafc6fc210f433b80016d1dc04aa748f5e
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2017-06-27 08:05:25 -0700

    Replace bash-it with custom bashrc etc.

--- bash-it/custom/030-direnv.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/local/env bash
-
-set -eu
-
-if [[ -f /usr/local/bin/direnv  || -f /usr/bin/direnv ]]; then
-  eval "$(direnv hook bash)"
-fi
-
-set +eu
--- bash-it/custom/600-yubishell.bash
+++
@@ -1,9 +0,0 @@
-yubishell() {
-  (
-    eval $(keychain --agents "gpg" --quiet --eval);
-    gpg-connect-agent updatestartuptty /bye;
-    export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
-    export PS1='\[\e[31m\][YUBI]\[\e[0m\] \$ ';
-    bash --norc
-  )
-}
