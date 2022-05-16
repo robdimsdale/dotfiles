8291aff0d5583e463981631bbdfd5956392d1ac7
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2022-05-16 10:26:17 -0400

    WIP rust nvim plugins

---
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.plugins = function(use)
+  -- Add your own plugins
+  use 'simrat39/rust-tools.nvim'
+  -- use 'kyazdani42/nvim-tree.lua'
+  -- use '~/my-prototype-plugin'
+  -- see :help packer for more options
+end
+
+return M
