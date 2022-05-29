7813362150965aa9c615b8043b800b206957ad1d
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2022-05-29 11:26:00 -0400

    Include hidden files in nvim fzf file search.

--- nvim/.config/nvim/lua/user/after.lua
+++ nvim/.config/nvim/lua/user/after.lua
@@ -8,3 +8,7 @@ autosave.setup(
 )
 
 lspconfig.pylsp.setup {}
+
+vim.schedule(function()
+    vim.env.FZF_DEFAULT_COMMAND = 'fd --hidden --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f'
+end)
